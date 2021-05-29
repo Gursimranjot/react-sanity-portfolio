4a101ad71ee372d8a9b4d36b4cbde8fd5a84c0d0
gursimran <simran23dhiman@gmail.com>
2021-05-29 21:03:53 +0530

    working on navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,27 @@
 import React from "react"
+import {NavLink, Navlink} from "react-router-dom"
 
 export default function Navbar() {
-        return <h1>Navbar Page</h1>
+        return(
+            <header className="header">
+                   <nav className="navbar">
+                        <div className="wrapper">
+                            <NavLink to="/" exact activeClassName="nav-active" className="nav-link">
+                                    Home
+                                </NavLink>
+                            <ul>
+                                <NavLink to="/post" className="nav-link" activeClassName="nav-active">
+                                    Blog Posts
+                                </NavLink>
+                                <NavLink to="/project" className="nav-link" activeClassName="nav-active">
+                                    Projects
+                                </NavLink>
+                                <NavLink to="/about" className="nav-link" activeClassName="nav-active">
+                                    About me
+                                </NavLink>
+                            </ul>
+                        </div>
+                   </nav>
+            </header>
+        )
 }
